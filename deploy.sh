67ca7abcaca3b9c44a1c9b7e37a9a9c2e181b731
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2019-01-30 17:36:06 +0800

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#/bin!bash
+
+TS=$(date +"%Y-%m-%d %H:%M:%S")
+dir_assets='ops-scripts/splunkforwarder'
+dir_inputs='/opt/splunkforwarder/etc/apps/search/local'
+dir_scripts='/opt/splunkforwarder/bin/scripts'
+
+git clone -b splunkforwarder-generic https://github.com/VoyagerInnovations/ops-scripts.git
+
+chmod +x $dir_assets/scripts/*sh
+
+if [ ! -d "$dir_inputs" ]; then
+        mkdir /opt/splunkforwarder/etc/apps/search/local/
+        cp $dir_assets/inputs.conf $dir_inputs/inputs.conf
+else
+        cp $dir_assets/inputs.conf $dir_inputs/inputs.conf
+fi
