296c4ef0b6dcf7f6d8afaf84d1a200a96f64b87d
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2019-01-31 10:37:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ dir_assets='ops-scripts/splunkforwarder'
 dir_inputs='/opt/splunkforwarder/etc/apps/search/local'
 dir_scripts='/opt/splunkforwarder/bin/scripts'
 
-git clone -b splunkforwarder-generic https://github.com/VoyagerInnovations/ops-scripts.git
 
 chmod +x $dir_assets/scripts/*sh
 
