7fa3d1eb890e4cdfeb7d0db9245545ae4794c422
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2019-02-13 15:04:53 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #/bin!bash
 
-TS=$(date +"%Y-%m-%d %H:%M:%S")
+TS=$(date +"%Y-%m-%d %H:%M:%S"
 dir_assets='ops-scripts/splunkforwarder'
 dir_inputs='/opt/splunkforwarder/etc/apps/search/local'
 dir_scripts='/opt/splunkforwarder/bin/scripts'
-
-git clone -b splunkforwarder-generic https://github.com/VoyagerInnovations/ops-scripts.git
 
 chmod +x $dir_assets/scripts/*sh
 
