2249bca95189b6218163b43cf13b7cb38c9ce17b
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2019-02-22 18:05:40 +0800

    updated date format
    by JG

--- splunkforwarder/scripts/supervisor.sh
+++ splunkforwarder/scripts/supervisor.sh
@@ -6,10 +6,10 @@ status=$?
 
 if [[ $status -eq 0 ]]; then
 	
-	date +"%F %T.%3N %z"
+	date +"%a %b %d %H:%M:%S.%3N %z %Y"
 	supervisorctl status
 	
 else
-
+	date +"%a %b %d %H:%M:%S.%3N %z %Y"
 	echo "NOSUPERVISOR NOSUPERVISOR There is no supervisorctl in this server"
 fi
