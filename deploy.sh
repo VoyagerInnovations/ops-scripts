e364aa0f160f1b950638d4bb38decbd320d1b9cf
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2020-09-02 05:58:43 +0800

    Update deploy.sh
    change dir_assets. 
    added splunk restart command

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #/bin!bash
 
-TS=$(date +"%Y-%m-%d %H:%M:%S"
-dir_assets='ops-scripts/splunkforwarder'
+TS=$(date +"%Y-%m-%d %H:%M:%S")
+dir_assets='splunkforwarder'
 dir_inputs='/opt/splunkforwarder/etc/apps/search/local'
 dir_scripts='/opt/splunkforwarder/bin/scripts'
 
 chmod +x $dir_assets/scripts/*sh
+cp $dir_assets/scripts/*sh $dir_scripts
 
 if [ ! -d "$dir_inputs" ]; then
         mkdir /opt/splunkforwarder/etc/apps/search/local/
@@ -13,3 +14,5 @@ if [ ! -d "$dir_inputs" ]; then
 else
         cp $dir_assets/inputs.conf $dir_inputs/inputs.conf
 fi
+
+/opt/splunkforwarder/bin/splunk restart
