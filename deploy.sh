25d83d77024b9f8a41fe810afd033c6e05226638
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2020-09-02 10:17:42 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 TS=$(date +"%Y-%m-%d %H:%M:%S")
 dir_assets='splunkforwarder'
-dir_inputs='/opt/splunkforwarder/etc/apps/search/local'
+dir_inputs='/opt/splunkforwarder/etc/apps/SplunkUniversalForwarder/local/'
 dir_scripts='/opt/splunkforwarder/bin/scripts'
 
 chmod +x $dir_assets/scripts/*sh
 cp $dir_assets/scripts/*sh $dir_scripts
 
 if [ ! -d "$dir_inputs" ]; then
-        mkdir /opt/splunkforwarder/etc/apps/search/local/
+        mkdir /opt/splunkforwarder/etc/apps/SplunkUniversalForwarder/local/
         cp $dir_assets/inputs.conf $dir_inputs/inputs.conf
 else
         cp $dir_assets/inputs.conf $dir_inputs/inputs.conf
