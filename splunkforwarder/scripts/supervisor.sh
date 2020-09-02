9150dd902dbaea4596981f35a2c7d0abebcf7856
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2020-09-02 09:52:46 +0800

    Update supervisor.sh

--- splunkforwarder/scripts/supervisor.sh
+++ splunkforwarder/scripts/supervisor.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-date
-supervisorctl status
+
+
+
+ supervisorctl status > /dev/null
+
+
+
+ status=$?
+
+
+
+ if [[ $status -eq 0 ]]; then
+
+
+
+ date +"%a %b %d %H:%M:%S.%3N %z %Y"
+
+ supervisorctl status
+
+
+else
+
+ date +"%a %b %d %H:%M:%S.%3N %z %Y"
+
+ echo "NOSUPERVISOR NOSUPERVISOR There is no supervisorctl in this server"
+
+ fi
