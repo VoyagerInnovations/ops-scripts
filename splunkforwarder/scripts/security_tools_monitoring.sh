359efdac5e2c92848d022277ad9ae322db882053
a-maynardprepotente <44601102+a-maynardprepotente@users.noreply.github.com>
2020-09-02 09:56:40 +0800

    Update security_tools_monitoring.sh

--- splunkforwarder/scripts/security_tools_monitoring.sh
+++ splunkforwarder/scripts/security_tools_monitoring.sh
@@ -21,6 +21,8 @@ service qualys-cloud-agent status 2> /dev/null | egrep '^qualys-cloud-agent\s\(p
 
 service ossec-hids status 2> /dev/null | egrep '^(ossec\-logcollector|ossec\-syscheckd|ossec\-agentd|ossec\-execd)\sis\srunning.+$' > ossec-processes.txt
 
+service ossec status 2> /dev/null | egrep '^(ossec\-logcollector|ossec\-syscheckd|ossec\-agentd|ossec\-execd)\sis\srunning.+$' > ossec-hids-processes.txt
+
 service clamd status 2> /dev/null | egrep '^clamd\s\(pid\s+[0-9]+\)\sis\srunning.+$' > clamd-processes.txt
 
 service clamd.scan status 2> /dev/null | egrep '^clamd\.scan\s\(pid\s+[0-9]+\)\sis\srunning.+$' > clamd-scan-processes.txt
@@ -35,7 +37,9 @@ CLAMD_SCAN_COUNT=`cat clamd-scan-processes.txt | wc -l`
 
 OSSEC_COUNT=`cat ossec-processes.txt | wc -l`
 
+OSSEC_HIDS_COUNT=`cat ossec-hids-processes.txt | wc -l`
+
 
 
 echo $EXEC_DATE_TIMESTAMP
-echo "QUALYST_PROCESS_COUNT=\"$QUALYST_COUNT\", OSSEC_PROCESS_COUNT=\"$OSSEC_COUNT\", CLAMD_PROCESS_COUNT=\"$CLAMD_COUNT\", CLAMD_SCAN_PROCESS_COUNT=\"$CLAMD_SCAN_COUNT\""
+echo "QUALYST_PROCESS_COUNT=\"$QUALYST_COUNT\", OSSEC_PROCESS_COUNT=\"$OSSEC_COUNT\", OSSEC_HIDS_PROCESS_COUNT=\"$OSSEC_HIDS_COUNT\", CLAMD_PROCESS_COUNT=\"$CLAMD_COUNT\", CLAMD_SCAN_PROCESS_COUNT=\"$CLAMD_SCAN_COUNT\""
